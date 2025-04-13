12c813fdde0be7db3b427385a19d592c5fae6155
KacenaS <simkova1995@outlook.com>
2025-04-13 12:32:21 +0200

    Fix: corrected settings module in manage.py

--- src/manage.py
+++ src/manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adventure_game.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shared_household.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
