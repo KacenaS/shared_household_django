ae3aea4deb56b5524fffc69d0c729ba32daf7ae5
KacenaS <simkova1995@outlook.com>
2025-05-08 13:46:18 +0200

    fixed redirect

--- src/shared_household/settings.py
+++ src/shared_household/settings.py
@@ -144,6 +144,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = 'login'  
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'homepage'
 
 
